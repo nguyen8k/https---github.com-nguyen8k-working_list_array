ff596ae7720ca4d473e1e5e15af82b26b738259d
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-04-24 10:16:34 -0600

    first commit

---
+++ working_list_array.py
@@ -0,0 +1,29 @@
+states_of_america = ["Delaware","Pennsylvannia","New York","Connecticut","Massachusetts","Mississippi","Utal"]
+
+# print(states_of_america)  # print all
+# print (states_of_america[-1])   # print the last item
+
+# states_of_america.append("Taxes")
+# print(states_of_america)               # add to the last position : Taxes
+
+# states_of_america.extend(["Hawai","Alaska","New Mexico"])
+# print (states_of_america)           # add 3 states to the end of the list
+
+num_of_america = len(states_of_america)
+#print ( states_of_america[num_of_america])
+
+city_name = [" San Jose"," Austin", " Boston", "Atlanta"," Dallas","Colorado", "Los Angeles"]
+
+city_name[-1] = "San Francisco"   # replace the last index Los Angeles = San Fransico
+city_name.append(" Houston")  # add Houston after San Franciso
+
+print (city_name)
+
+
+combination_items = [states_of_america,city_name]
+
+#print(combination_items)
+#print (combination_items[1][1])
+
+
+
